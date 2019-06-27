9bf0c3d3fc2f62d5b019408cdd00964a47864283
Darcy Jones <darcy.ab.jones@gmail.com>
2019-06-27 11:39:09 +0800

    added helper script to check that packages are installed

---
+++ scripts/check_packages.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import importlib
+
+requirements = [
+    "jupyter",
+    "pandas",
+    "numpy",
+    "matplotlib"
+]
+
+errors = []
+for requirement in requirements:
+    try:
+        importlib.import_module(requirement)
+    except ImportError as e:
+        errors.append(requirement)
+
+if len(errors) > 0:
+    print("Could not import the required package(s):")
+    print("-", "\n- ".join(errors))
+    print("Please ask one of our helpers to show you how to install them")
+else:
+    print("All of the packages are installed!")
+    print("You're good to go")
